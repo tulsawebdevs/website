e075582db650621f36dec6c445257ac7f70e2950
zenlex <erichrkeil@gmail.com>
2024-02-10 12:33:38 -0600

    ci: setup staging for dev on gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://tulsawebdevs.github.io',
+  base:'/website',
+  integrations: [tailwind()],
 });
