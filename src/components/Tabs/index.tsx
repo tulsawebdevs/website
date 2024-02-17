4b0c6d41c86d73265126b90d13568866b0b847a5
Alec Helmturner <alec@helmturner.dev>
2024-02-17 17:25:46 -0600

    chore: cleanup tab component

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,50 +1,30 @@
 import { useTabs } from '@zendeskgarden/container-tabs';
-import React, { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 
-export type UseTabsParams = Parameters<
+export type TabsProps = Parameters<
   typeof useTabs<{
     title: string;
     render: () => JSX.Element;
   }>
 >[0];
 
-export type TabsProps = Omit<UseTabsParams, 'focusedValue'> & {
-  focusedIndex?: number;
-};
-
-export default function Tabs(propsIn: TabsProps) {
-  const [focusedIndex, setFocusedIndex] = React.useState(
-    propsIn.focusedIndex || 0,
-  );
-
+export default function Tabs(props: TabsProps) {
   const { selectedValue, getTabListProps, getTabPanelProps, getTabProps } =
-    useTabs({
-      orientation: propsIn.orientation || 'horizontal',
-      tabs: propsIn.tabs,
-      focusedValue: propsIn.tabs[focusedIndex]?.value,
-      onFocus(focusedValue) {
-        setFocusedIndex(
-          propsIn.tabs.findIndex((tab) => tab.value === focusedValue),
-        );
-      },
-    });
-
-  useEffect(() => {
-    console.log('focusedIndex', propsIn.focusedIndex);
-  }, [propsIn.focusedIndex]);
+    useTabs(props);
 
   const components = useMemo(() => {
     const tabComponents = [];
     const tabPanelComponents = [];
 
-    propsIn.tabs.forEach(({ value }) => {
+    props.tabs.forEach(({ value }: { value: any }) => {
       tabComponents.push(
         <li
-          className={`3px solid ${value === selectedValue ? '#1f73b7' : 'transparent'} flex h-4 w-full cursor-pointer items-center justify-center`}
-          {...getTabProps({
-            value: value as any,
-            key: value?.title,
-          })}
+          {...getTabProps({ value })}
+          className={
+            value === selectedValue ?
+              `border-4 border-blue-600 border-solid flex h-4 w-full cursor-pointer items-center justify-center`
+            : `border-4 border-transparent border-solid flex h-4 w-full cursor-pointer items-center justify-center`
+          }
         >
           {value?.title}
         </li>,
@@ -52,10 +32,7 @@ export default function Tabs(propsIn: TabsProps) {
 
       tabPanelComponents.push(
         <div
-          {...getTabPanelProps({
-            value: value as any,
-            key: value?.title,
-          })}
+          {...getTabPanelProps({ value })}
           className="padding-top-10px padding-bottom-10px border-s-red-400"
         >
           {value?.render()}
@@ -64,7 +41,7 @@ export default function Tabs(propsIn: TabsProps) {
     });
 
     return { tabs: tabComponents, panels: tabPanelComponents };
-  }, [getTabProps, getTabPanelProps, propsIn.tabs, selectedValue]);
+  }, [getTabProps, getTabPanelProps, props.tabs, selectedValue]);
 
   return (
     <>
