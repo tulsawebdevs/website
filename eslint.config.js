e8b6fe91deaa5deda3cca48fd27d61931b653707
Alec Helmturner <alec@helmturner.dev>
2024-02-21 17:29:14 -0600

    chore: configure eslint for unicorn/airbnb

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview
+ * NOTE: While eslint claims to support .mjs extensions, they don't seem to work
+ */
+import * as eslintrc from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPrettier from 'eslint-config-prettier';
+
+const baseDirectory = import.meta.dirname;
+
+/** @see https://eslint.org/docs/latest/use/configure/migration-guide#using-eslintrc-configs-in-flat-config  */
+const compat = new eslintrc.FlatCompat({ baseDirectory });
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...compat.extends('eslint-config-airbnb', 'eslint-config-airbnb/hooks'),
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintPrettier,
+  {
+    rules: {
+      /**
+       * These rules are causing spurious errors related to ESLint not being
+       * albe to resolve files that TypeScript can resolve without issue
+       */
+      'import/no-unresolved': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+    },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: baseDirectory,
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+    },
+  },
+);
