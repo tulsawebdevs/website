60fada2f0e30956e1b104cbaef4770a526b960a0
Alec Helmturner <alec@helmturner.dev>
2024-02-21 18:18:51 -0600

    chore: fix type errors, refine tsconfig

--- src/ts-env.d.ts
+++ src/ts-env.d.ts
@@ -1,3 +1,13 @@
 /**
  * @fileoverview Augment modules or globals in this file, when necessary.
  */
+
+declare namespace NodeJS {
+  /**
+   * Fix errors from some untyped JS dependencies that use the unsound
+   * (but common) `['dep1', 'dep2'].map(require.resolve)` pattern.
+   */
+  interface RequireResolve {
+    (path: string): string;
+  }
+}
