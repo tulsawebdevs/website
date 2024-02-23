08d6b817137e3c233f251ba6be1ea6b37afe9360
zenlex <erichrkeil@gmail.com>
2024-02-23 13:20:16 -0600

    fix: remove base url and prefixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tulsawebdevs.github.io',
-  base: '/website',
   integrations: [tailwind()],
   server: {
     host: true,
