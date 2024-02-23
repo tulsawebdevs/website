68cfd4c89cd4ad3025b944a398ad84d131a390e5
Alec Helmturner <alec@helmturner.dev>
2024-02-22 19:20:37 -0600

    chore: add/configure code style toolchain (#17)
    * chore: add/configure formatter
    
    * style: format
    
    * chore: revert to whole-file format on save
    
    * Revert "style: format"
    
    This reverts commit 06525d57f4573bdb12d159eb6be56723cf733082.
    
    * chore: pin dependencies
    
    * chore: add TS style guide
    
    Pulled from https://github.com/Expensify/App
    
    * chore: add explicit typescript dependency
    
    * chore: allow comments in tsconfig
    
    TS doesn't choke on comments in tsconfig.json
    
    * style: make tsconfig slightly more strict
    
    * chore: customize TYPESCRIPT.md
    
    - removed referencces to expensify
    - removed/fixed parts that were irrelevent to this project
    - added 'single source of truth' file for globals/module augmentation
    
    * style: udpate prettier to use tabs, single quotes
    
    Also removes redundant options that were previously explicit
    see: https://techlahoma.slack.com/archives/C061ADYPHNJ/p1708476318174819
    
    * chore: cleanup prettier config
    
    * style: enable type acquisition
    
    * chore: remove temp comments
    
    * chore: fix typos in tsconfig/settings.json
    
    * chore: add linter dependencies
    
    * chore: configure eslint for unicorn/airbnb
    
    * chore: force using npm as package manager
    
    * chore: revert to spaces over tabs
    
    tabs reached consensus based on the misconception that 1 tab is 2 spaces
    
    * chore: add eslint extension to recommendations
    
    * chore: fix type errors, refine tsconfig
    
    * chore: add eslint script
    
    * chore: exclude node_modules from type checking
    
    * chore: add unicorn, fix lint config + eslint types
    
    * chore: refine eslint config, fix errors
    
    * chore: pin deps
    
    * chore: update workspace recommendations

---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+/**
+ * @fileoverview
+ * NOTE: While eslint claims to support .mjs extensions, they don't seem to work
+ */
+
+import * as eslintrc from '@eslint/eslintrc';
+import eslint from '@eslint/js';
+import unicorn from 'eslint-plugin-unicorn';
+import tseslint from 'typescript-eslint';
+import eslintPrettier from 'eslint-config-prettier';
+
+const baseDirectory = import.meta.dirname;
+
+/** @see https://eslint.org/docs/latest/use/configure/migration-guide#using-eslintrc-configs-in-flat-config  */
+const compat = new eslintrc.FlatCompat({ baseDirectory: import.meta.dirname });
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...compat.extends('eslint-config-airbnb', 'eslint-config-airbnb/hooks'),
+  {
+    ...unicorn.configs['flat/all'],
+    ignores: ['src/env.d.ts', 'src/ts-env.d.ts'],
+  },
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  eslintPrettier,
+  {
+    rules: {
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      /** These on by default rules DO NOT work well for this project */
+      'import/no-unresolved': 'off',
+      /** These off by default rules DO work well with this project */
+    },
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: baseDirectory,
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+    },
+  },
+);
