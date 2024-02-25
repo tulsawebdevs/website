131b950228c6e2f4cdbd0fc64a053e8140ab0b65
Erich Keil <39918654+zenlex@users.noreply.github.com>
2024-02-24 19:17:32 -0600

    Add Playwright Setup for e2e testing (#37)
    * feat: add playwright setup
    
    * fix: ignore playwright tests in vitest
    
    * feat: add playwright dep install to dockerfile cache
    
    * feat: add playwright job to ci flow

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
-FROM node:20
+FROM node:20-bookworm
 
 WORKDIR /app
 
 COPY package.json package-lock.json ./
 
 RUN npm ci
+RUN npx -y playwright@1.41.1 install --with-deps
 
 COPY . . 
 
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,11 @@ import { getViteConfig } from 'astro/config';
 export default getViteConfig({
   test: {
     globals: true,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/playwright/**',
+      '**/.{idea,git,cache,output,temp}/**',
+    ],
   },
 });
