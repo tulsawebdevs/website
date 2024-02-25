bae1068bf3fdfd703620a200368d5d5f4ad22c1f
Alec Helmturner <alec@helmturner.dev>
2024-02-24 19:01:46 -0600

    chore: fix merge error

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -5,7 +5,6 @@ import { useMemo } from 'react';
 type Tab<TValue extends string> = {
   value: TValue;
   title: string;
-  title: string;
   renderContent: () => JSX.Element;
   disabled?: boolean;
 };
