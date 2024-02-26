8ad385426d9d915a0f32110fdece5f25c488ef96
Alec Helmturner <alec@helmturner.dev>
2024-02-26 17:09:32 -0600

    chore: improve eslint config

--- src/ts-env.d.ts
+++ src/ts-env.d.ts
@@ -2,6 +2,9 @@
  * @fileoverview Augment modules or globals in this file, when necessary.
  */
 
+// Just to make ESLint happy, doesn't actually overwrite the global NoInfer type
+declare type NoInfer<A> = A;
+
 declare module 'eslint-plugin-unicorn' {
   import type { Linter } from 'eslint';
 
