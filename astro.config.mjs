0390662e60771f27780d0a5429c5e86ebb46a49a
Alec Helmturner <alec@helmturner.dev>
2024-02-27 15:25:44 -0600

    feat: add/configure vanilla-extract

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tulsawebdevs.github.io',
   integrations: [tailwind()],
+  vite: {
+    plugins: [vanillaExtractPlugin()],
+  },
   server: {
     host: true,
   },
---
+++ src/design/tokens/radius.css.ts
@@ -0,0 +1,17 @@
+// TODO: Values borrowed from Tamagui - Replace with actual design tokens
+export default {
+  0: 0,
+  1: 3,
+  2: 5,
+  3: 7,
+  4: 9,
+  true: 9,
+  5: 10,
+  6: 16,
+  7: 19,
+  8: 22,
+  9: 26,
+  10: 34,
+  11: 42,
+  12: 50,
+};
