069106a32466b7c78b7d6e819653c437eec62811
Alec Helmturner <alec@helmturner.dev>
2024-03-17 07:36:39 -0500

    refactor: simplify sign-in button

--- src/features/auth/SignInButton.tsx
+++ src/features/auth/SignInButton.tsx
@@ -1,33 +1,11 @@
-import { useRef, useEffect } from 'react';
 import { useClerk } from './hooks.ts';
-import useModal from '../navigation/useModal.tsx';
 
 export default function SignInButton() {
   const clerk = useClerk();
-  const signInElement = useRef<HTMLDivElement>(null);
-
-  const { Modal, close, open } = useModal({
-    onOpen: () =>
-      clerk?.isReady() &&
-      signInElement.current &&
-      clerk?.mountSignIn(signInElement.current),
-    onClose: () =>
-      signInElement.current && clerk?.unmountSignIn(signInElement.current),
-  });
-
-  useEffect(() => {
-    if (clerk?.user) close();
-    return () => close();
-  }, [clerk, close]);
 
   return (
-    <>
-      <button type="button" onClick={open}>
-        Sign in
-      </button>
-      <Modal>
-        <div ref={signInElement} />
-      </Modal>
-    </>
+    <button type="button" onClick={() => clerk?.openSignIn()}>
+      Sign in
+    </button>
   );
 }
