4995748a793863ee895a9c71f478927a7c0a7788
Alec Helmturner <alec@helmturner.dev>
2024-04-23 18:37:29 -0500

    fix: make tailwind config a module

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import animate from 'tailwindcss-animate';
+
+export default {
   darkMode: ['class'],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -33,5 +35,5 @@ module.exports = {
       },
     },
   },
-  plugins: [(await import('tailwindcss-animate')).default],
+  plugins: [animate],
 };
