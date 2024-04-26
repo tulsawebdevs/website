a7422caa1eceb3230fb3b1176b037f9b950d412b
RossaMania <raclettenberg@gmail.com>
2024-04-25 19:46:10 -0500

    Hotfix: Refactor Astro config to use @astrojs/react and comment out unused isDraft const in ProposalForm.tsx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import react from '@astrojs/renderer-react';
+// @ts-ignore
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://tulsawebdevs.github.io',
