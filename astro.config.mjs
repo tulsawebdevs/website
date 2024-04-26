9118b9b058933641e4e94cd68ec17ae206ce8f5f
RossaMania <raclettenberg@gmail.com>
2024-04-25 19:34:09 -0500

    Update Astro config to use @astrojs/renderer-react instead of @astrojs/react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import React from '@astrojs/react';
+import react from '@astrojs/renderer-react';
 
 // https://astro.build/config
 export default defineConfig({
