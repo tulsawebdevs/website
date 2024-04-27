55023020f97c6484175b862385e90f6197074a83
Alec Helmturner <alec@helmturner.dev>
2024-04-27 17:07:47 -0500

    fix: improve header coplors & contrast

--- src/features/auth/SignInButton.tsx
+++ src/features/auth/SignInButton.tsx
@@ -1,10 +1,15 @@
 import { useClerk } from './hooks.ts';
+import { signInButton } from './SignInButton.css.ts';
 
 export default function SignInButton() {
   const clerk = useClerk();
 
   return (
-    <button type="button" onClick={() => clerk?.openSignIn()}>
+    <button
+      type="button"
+      className={signInButton}
+      onClick={() => clerk?.openSignIn()}
+    >
       Sign in
     </button>
   );
--- src/features/navigation/Header.css.ts
+++ src/features/navigation/Header.css.ts
@@ -3,6 +3,7 @@ import { colors, font } from '../../design/tokens.css.ts';
 
 export const headerNav = style({
   fontFamily: font.ReneBieder2,
+  fontSize: '1.25rem',
   maxWidth: '800px',
   margin: 'auto',
   alignSelf: 'center',
@@ -13,12 +14,17 @@ export const headerNav = style({
   display: 'flex',
   flexDirection: 'row',
   justifyContent: 'space-between',
-  backgroundColor: colors['twd-blue-300'],
+  backgroundColor: colors['twd-blue-500'],
   paddingTop: '.5rem',
   paddingBottom: '.5rem',
   paddingLeft: '35px',
   paddingRight: '35px',
-  color: colors.black,
+  color: colors.white,
+  textShadow: `
+  1px 1px 0 #000, 
+  -1px 1px 0 #000,
+  -1px -1px 0 #000,
+  1px -1px 0 #000`,
   borderBottomLeftRadius: '8px',
   borderBottomRightRadius: '8px',
 });
