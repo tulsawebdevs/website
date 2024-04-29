0370df6be1e32f17de7156a554589c0eea0c884c
Alec Helmturner <alec@helmturner.dev>
2024-04-29 04:05:27 -0500

    fix: merge Proposal form components; better errors

--- src/components/ProposalFormErrors.tsx
+++ src/components/ProposalFormErrors.tsx
@@ -1,29 +1,41 @@
 /* eslint-disable max-classes-per-file */
-export class ProposalFormError extends Error {
-  constructor(cause: unknown) {
-    let message: string;
+import z from 'astro/zod';
 
-    if (cause instanceof Response) {
-      message = `${cause.status} Error: ${cause.statusText}`;
+export class ProposalFormError extends Error {
+  source: unknown;
+
+  constructor(source: unknown) {
+    let message: string | string[];
+    const og = source instanceof ProposalFormError ? source.source : source;
+
+    if (og instanceof Response) {
+      message = `${og.status} Error${og.statusText ? `: ${og.statusText}` : ''}`;
+    } else if (og instanceof z.ZodError) {
+      message = `${og.errors.map((err) => `${err.path.join('>')}: ${err.message}`).join('\n')}`;
+    } else if (typeof og === 'string') {
+      message = `${og}`;
+    } else if (og instanceof Error) {
+      message = `${og.message}`;
+    } else if (og instanceof ErrorEvent) {
+      message = `${og.message}`;
     } else {
-      message = `Unknown Error`;
+      message = 'An unknown error occurred';
     }
 
-    if (typeof cause === 'string') message += `: ${cause}`;
-    if (cause instanceof Error) message += `: ${cause.message}`;
-    if (cause instanceof ErrorEvent) message += `: ${cause.message}`;
-
     super(message);
+    this.source = source;
     this.name = 'ProposalFormError';
   }
 
   render() {
     return (
       <div className="first-line:font-bold space-y-2">
-        <p>{this.message}</p>
+        <p>Submission Failed</p>
         <p>
-          Please try again. If the problem continues, please contact support.
+          There was an error submitting your proposal. Please try again. If the
+          problem continues, please contact support.
         </p>
+        <p>{this.message}</p>
       </div>
     );
   }
