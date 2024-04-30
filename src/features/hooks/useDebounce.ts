b634181e1a86d50ff067f73eea4e6757350d501b
ng <n.guerns@gmail.com>
2024-04-30 15:04:27 -0500

    chore: update typings

--- src/features/hooks/useDebounce.ts
+++ src/features/hooks/useDebounce.ts
@@ -1,18 +1,14 @@
 import { useCallback, useRef } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type FunctionArgs<Args extends any[] = any[], Return = void> = (
-  ...args: Args
-) => Return;
-
-export default function useDebounceFn<T extends FunctionArgs>(
-  callback: T,
+export default function useDebounceFn<A extends any[], R = void>(
+  callback: (...args: A) => R,
   delay = 300,
 ) {
   const timer = useRef<ReturnType<typeof setTimeout>>();
 
   const debouncedCallback = useCallback(
-    (...args: unknown[]) => {
+    (...args: A) => {
       clearTimeout(timer.current);
 
       timer.current = setTimeout(() => {
