08d0e52a339936ea6481975c96cc38e466ce0a5a
Alec Helmturner <alec@helmturner.dev>
2024-05-11 18:45:39 -0500

    types: export more accurate clerk types
    - Asserts required user properties as non-null

--- src/features/auth/clerk.ts
+++ src/features/auth/clerk.ts
@@ -1,4 +1,4 @@
-/** // ! Note:
+/**
  * Importing '@clerk/clerk-js' with a default export doesn't work during development,
  * but works when the app is built for production. Conversely, importing Clerk as a
  * named export works during development, but not in production. For this reason,
@@ -10,16 +10,35 @@ import { atom } from 'nanostores';
 import { colors, font } from '../../design/tokens.css';
 import { PUBLIC_CLERK_PUBLISHABLE_KEY } from '../../constants.ts';
 
+type ClerkUser = NonNullable<ClerkJs.Clerk['user']>;
+type ClerkSession = NonNullable<ClerkJs.Clerk['session']>;
+
+// Customize types to reflect the settings in the Clerk dashboard
+export type User = NonNullable<ClerkUser> & {
+  firstName: string;
+  lastName: string;
+  primaryEmailAddress: NonNullable<ClerkUser['primaryEmailAddress']>;
+};
+
+export type Session = NonNullable<ClerkSession> & { user: User };
+
+export type Clerk = ClerkJs.Clerk & {
+  user: User | null | undefined;
+  session: Session | null | undefined;
+};
+
+export type AuthConditions = Parameters<Session['checkAuthorization']>[0];
+
 const { Clerk } = ClerkJs;
 const CLERK_PUBLIC_KEY = PUBLIC_CLERK_PUBLISHABLE_KEY;
 
-export const clerkStore = atom<ClerkJs.Clerk | null>(null);
+export const clerkStore = atom<Clerk | null>(null);
 
 export const initializeClerk = async () => {
   if (clerkStore.get()) return; // Already initialized
   if (!CLERK_PUBLIC_KEY) throw new Error('Clerk public key is not set');
 
-  const clerk = new Clerk(CLERK_PUBLIC_KEY);
+  const clerk = new Clerk(CLERK_PUBLIC_KEY) as Clerk;
   await clerk.load({
     supportEmail: 'tulsawebdevs@techlahoma.org',
     appearance: {
