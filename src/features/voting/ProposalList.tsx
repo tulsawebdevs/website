e7699fe110898427c296ee130ee7bd860f68d509
ng <n.guerns@gmail.com>
2024-06-12 11:29:45 -0500

    fix: hide load more when no proposals remaining

--- src/features/voting/ProposalList.tsx
+++ src/features/voting/ProposalList.tsx
@@ -5,12 +5,13 @@ import ProposalFormButton from './ProposalFormButton.tsx';
 import ProposalCard, { type ProposalCardProps } from './ProposalCard.tsx';
 import { Button } from '../ui/button.tsx';
 import { sdk, type Paginated } from '../../sdk.ts';
+import { LoadingSpinner } from '../ui/LoadingSpinner.tsx';
 
 // This component auto-loads proposals on scroll, so we hard-code a static limit
 const limit = 10;
 
 export function ProposalList() {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [cursor, setCursor] = useState<Paginated['cursor']>();
   const [proposals, setProposals] = useState<ProposalCardProps[]>([]);
 
@@ -41,24 +42,34 @@ export function ProposalList() {
     <div>
       <ul className="flex flex-col" id="proposal-list">
         {proposals.map((proposal) => (
-          <li key={`${proposal.summary}`} className="mb-2">
+          <li key={`${proposal.id}`} className="mb-2">
             {/* eslint-disable-next-line react/jsx-props-no-spreading */}
             <ProposalCard {...proposal} />
           </li>
         ))}
-        {loading || proposals.length ?
+
+        {loading && (
+          <div className="flex justify-center p-5 text-black">
+            <LoadingSpinner />
+          </div>
+        )}
+
+        {!loading && cursor && (
           <div className="flex justify-center p-5 text-black">
             <Button onClick={onClick} busy={loading} variant="secondary">
               Load More
             </Button>
           </div>
-        : <li>
+        )}
+
+        {!loading && proposals.length === 0 && (
+          <li>
             <div className="text-center p-2">
               <h2 className="font-bold text-xl mb-2">No proposals found</h2>
               <ProposalFormButton />
             </div>
           </li>
-        }
+        )}
       </ul>
     </div>
   );
