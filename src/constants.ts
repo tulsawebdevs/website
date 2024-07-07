bce5df0ef0fb657e8c4e8190ad834ae00e31405a
Alec Helmturner <alec@helmturner.dev>
2024-07-07 13:10:36 -0500

    chore: switch to production clerk instance

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,4 @@
 export const PUBLIC_CLERK_PUBLISHABLE_KEY =
-  'pk_test_ZW5vcm1vdXMtYnVnLTMxLmNsZXJrLmFjY291bnRzLmRldiQ';
+  process.env.NODE_ENV === 'production' ?
+    'pk_live_Y2xlcmsudHVsc2F3ZWJkZXZzLm9yZyQ'
+  : 'pk_test_ZW5vcm1vdXMtYnVnLTMxLmNsZXJrLmFjY291bnRzLmRldiQ';
