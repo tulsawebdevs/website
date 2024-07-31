9ee069d6e824af5b9509f09adb77eb601492f056
ng <n.guerns@gmail.com>
2024-07-31 15:52:52 -0500

    test: setup testing for react components

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,17 @@
 import { getViteConfig } from 'astro/config';
+import react from '@vitejs/plugin-react';
 
 export default getViteConfig({
+  plugins: [react()],
   test: {
     globals: true,
+    environment: 'jsdom',
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
       '**/playwright/**',
       '**/.{idea,git,cache,output,temp}/**',
     ],
+    setupFiles: './tests/vitest.setup.ts',
   },
 });
